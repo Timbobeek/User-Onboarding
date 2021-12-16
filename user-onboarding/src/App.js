9c34c121ec4f0cef1bd23308d0e3cfa2fec2d766
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-12-15 19:54:13 -0600

    added useEffect to get users and put them in state

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -4,9 +4,10 @@ import styled from 'styled-components';
 
 import Form from './Form';
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const StyledApp = styled.div`
-color: red;
+color: brown;
 background-color: beige;
 display: flex;
 text-align: center;
@@ -27,6 +28,19 @@ function App() {
   const [users, setUsers] = useState([]);
 
   const [formValues, setFormValues] = useState(initialFormValues);
+
+  const getUsers = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(response => {
+        //console.log(response.data)
+        setUsers(response.data)
+      })
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])   ///we need useEffect for getUsers to work
+
 
   const formSubmit = () => {
     //alert('11111');
